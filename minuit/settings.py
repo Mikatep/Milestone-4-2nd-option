28617b103de4b2de24b541a7bfd48222df44d9c5
Mikatep <miriamcordobacapote@gmail.com>
2022-09-06 20:38:18 +0000

    changes settings

--- minuit/settings.py
+++ minuit/settings.py
@@ -192,6 +192,6 @@ DEFAULT_FROM_EMAIL = 'minuit@example.com'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
